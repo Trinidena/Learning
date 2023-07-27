bd9fdde14dfd92ccbba09e1f54ff91b186d18c1d
Trinidad M Dena <deitychomp@yahoo.com>
2023-07-26 21:44:49 -0400

    adding first-app

---
+++ Angular/first-app/src/app/housing.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class HousingService {
+
+  constructor() { }
+
+}
---
+++ Angular/first-app/src/main.ts
@@ -0,0 +1,9 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+
+bootstrapApplication(AppComponent,
+  {providers: [provideProtractorTestingSupport()]})
+.catch(err => console.error(err));
