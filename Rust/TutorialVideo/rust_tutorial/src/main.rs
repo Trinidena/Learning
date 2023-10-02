2271e940abf04e68c609dad6ec1817870b727961
Trinidad M Dena <deitychomp@yahoo.com>
2023-10-02 11:03:04 -0400

    updated rust practice

--- Rust/TutorialVideo/rust_tutorial/src/main.rs
+++ Rust/TutorialVideo/rust_tutorial/src/main.rs
@@ -107,4 +107,18 @@ fn main() {
         Ordering::Greater => println!("Can Vote"),
         Ordering::Equal => println!("You just gained the right to vote!"),
     };
+
+    println!("\n\n");
+
+    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("1st Index Value: {}", arr_1[0]);
+    println!("Length of arr_1: {}", arr_1.len());
+
+    let mut loop_idx = 0;
+    loop {
+        if arr_1[loop_idx] % 2 != 0 {
+            println!("Value: {}", arr_1[loop_idx]);
+        }
+        loop_idx += 1;
+    }
 }
