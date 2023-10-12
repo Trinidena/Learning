8172e3c435921d4c62e97b2f8d6f907542abbfed
Trinidad M Dena <deitychomp@yahoo.com>
2023-10-12 16:14:38 -0400

    .

--- Rust/TutorialVideo/rust_tutorial/src/main.rs
+++ Rust/TutorialVideo/rust_tutorial/src/main.rs
@@ -7,8 +7,10 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
+    say_hello();
+    get_sum(5, 4);
+    println!("{}", get_sum2(2, 2));
     println!("What is your name?");
-
     let mut name = String::new();
     let greeting = "Nice to meet you";
     io::stdin()
@@ -110,15 +112,171 @@ fn main() {
 
     println!("\n\n");
 
-    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     println!("1st Index Value: {}", arr_1[0]);
     println!("Length of arr_1: {}", arr_1.len());
 
     let mut loop_idx = 0;
+
     loop {
-        if arr_1[loop_idx] % 2 != 0 {
+        if arr_1[loop_idx] % 2 == 0 {
             println!("Value: {}", arr_1[loop_idx]);
+            loop_idx += 1;
+            continue;
+        }
+        if arr_1[loop_idx] == 9 {
+            println!("Limit reached.");
+            break;
         }
         loop_idx += 1;
     }
+
+    while loop_idx < arr_1.len() {
+        println!("Arr: {}", arr_1[loop_idx]);
+        loop_idx += 1;
+    }
+
+    for val in arr_1.iter() {
+        println!("Val: {}", val);
+    }
+
+    let my_tuple: (u8, String, f64) = (47, "Derek".to_string(), 50_000.0);
+
+    println!("Name: {}", my_tuple.1);
+
+    let (v1, v2, v3) = my_tuple;
+    println!("Age: {}", v1);
+
+    let mut st1 = String::new();
+    st1.push('A');
+    st1.push_str("word");
+
+    for word in st1.split_whitespace() {
+        println!("{}", word);
+    }
+
+    let st2 = st1.replace("A", "Another");
+    println!("{}", st2);
+
+    let st3 = String::from(" x r t b h k k a m c");
+
+    let mut v4: Vec<char> = st3.chars().collect();
+    v4.sort();
+    v4.dedup();
+
+    for char in v4 {
+        println!("{}", char);
+    }
+
+    let st4: &str = "Random String";
+    let mut st5 = st4.to_string();
+    println!("{}", st5);
+
+    let byte_arr1 = st5.as_bytes();
+
+    let st6 = &st5[0..6];
+
+    println!("String length: {}", st6.len());
+
+    st5.clear();
+
+    let st6 = String::from("Just some");
+    let st7 = String::from(" words");
+    let st8 = st6 + &st7;
+
+    for char in st8.bytes() {
+        println!("{}", char);
+    }
+
+    let int_u8: u8 = 5;
+    let int2_u8: u8 = 4;
+
+    let in3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
+
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+
+    impl Day {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true,
+                _ => false,
+            }
+        }
+    }
+
+    let today: Day = Day::Monday;
+    let today0: Day = Day::Saturday;
+    let today1: Day = Day::Sunday;
+
+    match today {
+        Day::Monday => println!("Everyone hates Monday"),
+        Day::Tuesday => println!("Donut day"),
+        Day::Wednesday => println!("Hump day"),
+        Day::Thursday => println!("Pay day"),
+        Day::Friday => println!("Almost Weekend"),
+        Day::Saturday => println!("Weekend"),
+        Day::Sunday => println!("Weekend"),
+    }
+    println!("Is today the weekend? {}", today.is_weekend());
+    println!("Is today the weekend? {}", today0.is_weekend());
+    println!("Is today the weekend? {}", today1.is_weekend());
+
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2 = vec![1, 2, 3, 4];
+    vec2.push(5);
+    println!("1st: {}", vec2[0]);
+
+    let second: &i32 = &vec2[1];
+
+    match vec2.get(1) {
+        Some(second) => println!("2nd: {}", second),
+        None => println!("No 2nd value"),
+    }
+
+    for i in &mut vec2 {
+        *i *= 2;
+    }
+
+    for i in &vec2 {
+        println!("{}", i);
+    }
+    println!("Vec Length: {}", vec2.len());
+    println!("Pop: {:?}", vec2.pop());
+
+    fn say_hello() {
+        println!("Hello");
+    }
+
+    fn get_sum(x: i32, y: i32) {
+        println!("{} + {} = {}", x, y, x + y);
+    }
+    fn get_sum2(x: i32, y: i32) -> i32 {
+        return x + y;
+    }
+    fn get_2(x: i32) -> (i32, i32) {
+        return (x + 1, x + 2);
+    }
+
+    fn sum_list(list: &[i32]) -> i32 {
+        let mut sum = 0;
+        for &val in list.iter() {
+            sum += &val;
+        }
+        sum
+    }
+
+    let (val1, val2) = get_2(1);
+    println!("Val1: {} , Val2: {}", val1, val2);
+
+    let num_list = vec![1, 2, 3, 4, 5];
+
+    println!("Sum of list = {}", sum_list(&num_list));
 }
